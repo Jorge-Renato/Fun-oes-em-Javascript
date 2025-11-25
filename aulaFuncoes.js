2c8027fe6f78dee9ccf1879f3c33064b9a90916b
Jorge-Renato <jorgenato@hotmail.com>
2025-11-24 23:09:19 -0300

    feat: inicia projeto de estudo de funcoes em JavaSscript

---
+++ aulaFuncoes.js
@@ -0,0 +1,39 @@
+// function digaQualnome(nome) {
+//     console.log(nome)
+// }
+// digaQualnome("João")
+// digaQualnome("Jorge")
+// digaQualnome("Julia")
+
+// nessa funcão temos dois parâmetros: num1 e numero2
+// para criar a sintexe temos a palavra reservada function, o nome da função(calculo), os parênteses com os parâmetros(num1, numero2)
+//  e as chaves que delimitam o corpo da função { ... }
+
+// Dentro do corpo da função temos a lógica que nesse caso é somar os dois números e armazenar o resultado na variável resultado
+
+// Depois usamos o console.log para imprimir o valor da variável resultado no console.
+
+// function calculo(num1, num2) {
+//     let resultado = num1 + num2
+//     console.log(resultado)
+// }
+// calculo(3, 5)
+
+// essa é uma function com parametros e sem retorno
+
+//------------------------------------------------------------------------------------------------------------------
+
+// function saudacao(){
+//     console.log ("Ola mundo!")
+// }
+// saudacao()
+
+// function mostrarCurso() {
+//     console.log("Curso: Desenvolvimento em JavaScript")
+// }
+// mostrarCurso();
+
+function mensagemMotivacional(){
+    console.log("Voce é capaz de aprender qualquer coisa!")
+}
+mensagemMotivacional();
